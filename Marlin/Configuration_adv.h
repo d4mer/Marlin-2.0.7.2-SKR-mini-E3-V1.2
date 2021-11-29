60699cfbf5530e4e57aeca54f3d519c7ddf69220
D4mer <d4mer@d4mations.com>
2021-11-29 18:14:13 +0100

    disabled beizer

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1812,7 +1812,7 @@
 #endif
 
 // Support for G5 with XYZE destination and IJPQ offsets. Requires ~2666 bytes.
-#define BEZIER_CURVE_SUPPORT
+//#define BEZIER_CURVE_SUPPORT
 
 /**
  * Direct Stepping
